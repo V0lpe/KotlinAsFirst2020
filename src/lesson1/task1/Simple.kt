30dd0c99341a74fac128918ab13386680fff60de
alekseivoroshlov <nekobeardetected@gmail.com>
2020-09-08 17:35:02 +0300

    Tasks solved: numberRevert,thirdDigit

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -85,6 +85,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
  */
 fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
 
+
 /**
  * Тривиальная (1 балл)
  *
@@ -103,7 +104,8 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int = number / 100 % 10
+
 
 /**
  * Простая (2 балла)
@@ -134,4 +136,5 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int = number % 10 * 100 + number / 10 % 10 *10 + number / 100 % 10
+
